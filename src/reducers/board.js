48426c0e4d46ae49c09c3a89f7a97cf23c8c8f08
Alan Sutherland <sutherland_a@me.com>
2019-02-03 17:25:44 +0900

    Board initial

--- src/reducers/board.js
+++ src/reducers/board.js
@@ -1,39 +1,41 @@
-import { declareWinner } from '../actions/winner'
+import { declareWinner } from "../actions/winner";
 
 const initial = {
-    player: 'red',
-    board: [
-        ['','','','','','',''], // 0
-        ['','','','','','',''], // 1 
-        ['','','','','','',''], // 2
-        ['','','','','','',''], // 3
-        ['','','','','','',''], // 4
-        ['','','','','','',''], // 5
-        ['','','','','','',''], // 6
-    ]
-}
+  player: "red",
+  board: [
+    ["", "", "", "", "", "", ""], // 0
+    ["", "", "", "", "", "", ""], // 1
+    ["", "", "", "", "", "", ""], // 2
+    ["", "", "", "", "", "", ""], // 3
+    ["", "", "", "", "", "", ""], // 4
+    ["", "", "", "", "", "", ""], // 5
+    ["", "", "", "", "", "", ""] // 6
+  ]
+};
 
 const board = (state = initial, action) => {
+  if (action.type === "NEW_GAME") {
+    return { board: initial.board };
+  }
 
-    if (action.type === 'NEW_GAME') {
-        return { ...initial }
-    }
+  if (action.type === "SLOT_SELECTED") {
+    // console.log('Slot selected on column ' + action.column)
+    const slot = state.player;
+    const newBoardState = state.board.slice();
+    const col = newBoardState[action.column];
+    const empty = col.findIndex(e => e === "");
+    col.pop(col.length - 1);
+    col.splice(empty, "", slot);
 
-    if (action.type === 'SLOT_SELECTED') {
-        // console.log('Slot selected on column ' + action.column)
-        const slot = state.player;
-        const col = state.board[action.column]
-        const empty = col.findIndex(e => e === '');
-        col.splice(empty, '', slot)
-        const board = state.board.slice()
-        board[action.column] = col
-        return{
-            player: state.player === 'red' ? 'yellow' : 'red',
-            board: board
-        }
-    } 
-    
-    return state;
-}
+    newBoardState[action.column] = col;
+    console.log(state.board);
+    console.log(newBoardState);
+    return {
+      player: state.player === "red" ? "yellow" : "red",
+      board: newBoardState
+    };
+  }
+  return state;
+};
 
-export default board
+export default board;
--- src/reducers/winner.js
+++ src/reducers/winner.js
@@ -1,12 +1,16 @@
-const defaultWinnerState = "";
+const winnerState = "";
 
-const winner = (prevState = defaultWinnerState, action) => {
+const winner = (prevState = winnerState, action) => {
   if (action.type === "DECLARE_WINNER") {
     return action.player;
   }
 
   if (action.type === "DECLARE_TIE") {
     return 'Tie';
+  }
+
+  if (action.type === 'NEW_GAME') {
+    return winnerState
   }
 
   return prevState;
